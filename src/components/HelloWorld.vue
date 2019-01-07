ef5283c0909e6d02fca91de8c42a47b0d8106418
alberto <albertodeagostini88@gmail.com>
2019-01-07 21:56:29 +0100

    first commit, created the logic to import and filter the dictionary

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,107 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <v-container>
+    <v-layout
+      text-xs-center
+      wrap
+    >
+
+      <v-flex xs12>
+        <v-checkbox label="Qualsiasi lunghezza" v-model="undefinedLength"></v-checkbox>
+        <v-text-field label="Inserisci una parola con degli * dove non sai che lettere mettere" v-model="pattern"
+        ></v-text-field>
+        <v-btn @click="searchWord">Search</v-btn>
+      </v-flex>
+
+
+      <!-- <v-flex mb-5 xs12>
+        <h2 class="headline font-weight-bold mb-3">What's next?</h2>
+        <v-layout justify-center>
+          <a
+            v-for="(next, i) in whatsNext"
+            :key="i"
+            :href="next.href"
+            class="subheading mx-3"
+            target="_blank"
+          >
+            {{ next.text }}
+          </a>
+        </v-layout>
+      </v-flex> -->
+
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  import dictIta from "../assets/ita-dict.json";
+
+  export default {
+    data: () => ({
+      dictionary: null,
+      pattern: "",
+      undefinedLength: false,
+
+      whatsNext: [
+        {
+          text: 'Explore components',
+          href: 'https://vuetifyjs.com/components/api-explorer'
+        },
+        {
+          text: 'Select a layout',
+          href: 'https://vuetifyjs.com/layout/pre-defined'
+        },
+        {
+          text: 'Frequently Asked Questions',
+          href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions'
+        }
+
+      ]
+    }),
+
+    methods: {
+      searchWord() {
+        console.log(this.pattern);
+        const availableWords = [];
+        const map = {};
+        const letters = this.pattern.split("");
+        const length = letters.length;
+        let counter = 0;
+
+        // filter by length to relieve a bit
+        const words = this.undefinedLength ? this.dictionary : this.dictionary.filter( a => a.length === length);
+        console.log("remained " + words.length + " words");
+
+        letters.forEach((letter, index) => {
+          if(letter !== "_" && letter !== "*") {
+            map[index] = letter;
+          }
+        });
+
+        words.forEach( word => {
+          let mismatch = false;
+          Object.keys(map).forEach( key => {
+            if(word[key] !== letters[key]) {
+              mismatch = true;
+            }
+          });
+
+          if(!mismatch)
+            availableWords.push(word);
+
+          ++counter;
+        });
+
+        console.log("checked for " + counter + " words");
+        console.log(availableWords);
+      }
+    },
+
+    created() {
+      this.dictionary = dictIta.dictionary;
+    }
   }
-}
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+<style>
+
 </style>
